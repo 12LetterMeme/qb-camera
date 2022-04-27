e9ced2fcd272c01c6c7bc24c9f7c7b7436f72396
12LetterMeme <91357757+12LetterMeme@users.noreply.github.com>
2022-04-27 14:13:35 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -10,10 +10,3 @@ end)
 QBCore.Functions.CreateCallback("TLM:WEBHOOKYO",function(source,cb)
 	cb(WebHook)
 end)
-
---[[  For Shared.. obv
-
-["dslrcamera"] 		 	 		 = {["name"] = "dslrcamera", 					["label"] = "PD Camera", 				["weight"] = 1000, 	["type"] = "item", 			["image"] = "camera.png", 				["unique"] = true, 		["useable"] = true, 		["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "DSLR Camera, with cloud uplink.. cool right?"},
-
-]]
-
