813319cf1fcd832de7352834cd14bda19d4984e7
12LetterMeme <91357757+12LetterMeme@users.noreply.github.com>
2022-05-02 19:43:51 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 author '12LetterMeme#0001'
 description 'Gives the ability for police to document crime scenes or collect evidence'
-version '1.0.3'
+version '1.0.4'
 
 client_scripts { 'client.lua' }
 server_scripts { 'server.lua' }
