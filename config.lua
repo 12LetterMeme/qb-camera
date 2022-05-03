4483b197ea125c3fb3248f917a21733df2e37797
12LetterMeme <91357757+12LetterMeme@users.noreply.github.com>
2022-05-02 20:06:21 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,7 @@
 Config = Config or {}
 
 Config.Webhook = "" --Toss in your amazing webhook
+
 Config.MaxFOV = 90 --How wide the FOV is (fisheye)
 Config.MinFOV = 10 --How cropped the FOV is (zoom)
 Config.ZoomSpeed = 5 -- camera zoom speed
